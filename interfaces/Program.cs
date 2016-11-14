c4fdac968adedab2377d7502b9fe30bfc6fbcafc
Paul <pangambu@yahoo.com>
2016-11-14 18:02:47 -0500

    Interfaces Begining

--- interfaces/Program.cs
+++ interfaces/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace interfaces
 {
-    class Program
+    class Program : IName , ICity
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Using interfaces to print name and city");
+
+            IName refname = new Program();
+            ICity refCity = new Program();
+            refname.PrintName();
+            refCity.PrintCity();
+
+        
+
+
+            Console.ReadKey();
+
         }
+
+
+        public void PrintName()
+        {
+            Console.WriteLine("Mark");
+        }
+
+        public void PrintCity()
+        {
+            Console.WriteLine("Boston");
+        }
+    }
+
+    interface IName
+    {
+        void PrintName();
+    }
+
+    interface ICity
+    {
+        void PrintCity();
     }
 }
